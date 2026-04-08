8e181ba46ed16cedd1a336cc9d385d17ac63030c
dipesh paneru <dipeshpaneru1997@gmail.com>
2026-04-08 12:48:17 -0400

    Fine tuned evaluation

--- src/models.py
+++ src/models.py
@@ -1,4 +1,5 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
+from peft import PeftModel
 import torch
 
 MODEL_NAME = "TinyLlama/TinyLlama_v1.1"
@@ -19,3 +20,26 @@ def get_base_model():
     print(f"Device: {next(model.parameters()).device}")
 
     return(model, tokenizer)
+
+
+def get_fine_tuned_model():
+
+    # ADAPTER_DIR is the path of the adapter in Google Drive. Used in Google Colab.
+    ADAPTER_DIR = "/content/drive/MyDrive/Course-Assistant/outputs/qlora/final_adapter"
+
+    bnb_config = BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_quant_type="nf4",
+        bnb_4bit_compute_dtype=torch.bfloat16,
+    )
+
+    base = AutoModelForCausalLM.from_pretrained(
+        "TinyLlama/TinyLlama_v1.1",
+        quantization_config=bnb_config,
+        device_map="auto",
+    )
+
+    ft_model  = PeftModel.from_pretrained(base, ADAPTER_DIR)
+    ft_tokenizer = AutoTokenizer.from_pretrained(ADAPTER_DIR)
+    
+    return ft_model, ft_tokenizer
